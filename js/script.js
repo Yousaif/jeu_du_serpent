defc241c0700a1cdcec1df9f5f561d42de175fa1
Yousaif Dion <YoussaifDion@hotmail.com>
2020-11-20 16:03:56 -0500

    Premier commit sur l'exercice du serpent avec application des classes constructor.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event)
+{
 
+    /*Code pour le jeu*/
+
+    class Jeu
+    {
+        constructor()
+        {
+            console.log("Jeu");
+        }
+    }
+
+    /*Code pour le serpent*/
+
+    class Serpent
+    {
+        constructor()
+        {
+            console.log("Serpent");
+        }
+
+    }
+
+    /*Code pour la pomme*/
+
+    class Pomme
+    {
+        constructor()
+        {
+            console.log("Pomme");
+        }
+
+    }
+
+    var Partie = new Jeu();
 
 });
